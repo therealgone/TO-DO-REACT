2b71c1766489bdc3586f52ebf272e508b374f617
Jeevan Baabu <jeevanbaabu03@gmail.com>
2025-06-23 20:40:39 +0400

    to do made

--- to-do-react/src/app/todo/page.tsx
+++ to-do-react/src/app/todo/page.tsx
@@ -1,17 +1,59 @@
-import { useState } from "react"
+"use client";
+import { useState } from "react";
+
+export default function Todo() {
+
+  const [input, SetInput] = useState("")
+  const [todo, SetTodo] = useState<string[]>([])
+
+  const handelInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    SetInput(e.target.value)
+  }
+
+  const Enter =(e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == "Enter"){
+      AddTodo()
+    }
 
 
+  }
 
-export default function todo () {
-    return(
-            <div className="bg-black min-h-screen ">
-      
-                
-                e
-         <input
-         placeholder="enter the text"
-         ></input>
-        </div>
-    );
+  const AddTodo = () => {
+    if (input.trim() === "")
+      return
+    SetTodo([...todo, input])
     
+    SetInput("")
+   
+  }
+
+  return (
+    <div className="bg-black min-h-screen text-white ">
+      <input
+        placeholder="enter the text"
+        type="text"
+        value={input}
+        onChange={handelInputChange}
+        onKeyDown={Enter}
+
+
+      ></input>
+
+      <button
+        onClick={AddTodo}
+        
+      > add</button>
+
+      <ul className="text-white">
+        {todo.map((todo, index) => (
+        <li className="text-white">
+          {todo}
+        </li>
+        ))}
+
+      </ul>
+
+
+    </div>
+  );
 }
