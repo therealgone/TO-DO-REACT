4c7b56e3b48e17f4b385c9b88c0a9d27be1476fd
Jeevan Baabu <jeevanbaabu03@gmail.com>
2025-06-28 10:49:43 +0400

    TASK DONE 1/2 DONE

--- to-do-react/src/app/todo/page.tsx
+++ to-do-react/src/app/todo/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function Todo() {
   const [input, SetInput] = useState("");
   const [todo, SetTodo] = useState<string[]>([]);
+  const [clicked, SetClicked] = useState<number>(-1);
 
   const handelInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     SetInput(e.target.value);
@@ -20,6 +21,11 @@ export default function Todo() {
     SetTodo(updatedTodos);
   };
 
+  const handleClick = (index: number) => {
+    SetClicked(index);
+  };
+
+
   const AddTodo = () => {
     if (input.trim() === "") return;
     SetTodo([...todo, input]);
@@ -28,13 +34,13 @@ export default function Todo() {
 
   return (
     <div className="bg-black min-h-screen text-white overflow-y-auto relative">
-      
+
       {/* Top Header */}
       <div className="fixed top-0 left-0 w-full z-20 p-5 flex justify-between items-center bg-black bg-opacity-80">
         <h1 className="text-2xl font-bold tracking-widest text-shadow-[0_0_20px_white]">
           TO - DO - REACT
         </h1>
-        <nav className="space-x-5">
+        <nav className="space-x-5 font-bold">
           <a
             href="#"
             className="text-black bg-white px-4 py-2 rounded-3xl shadow-[0_0_10px_white] transition-all duration-300 ease-in-out 
@@ -47,7 +53,7 @@ export default function Todo() {
 
       {/* Main Content */}
       <div className="pt-[100px] flex flex-col items-center justify-start gap-6 px-4">
-        
+
         {/* Input & Button */}
         <div className="flex flex-row items-center gap-4 w-full max-w-md">
           <input
@@ -61,7 +67,7 @@ export default function Todo() {
           <button
             onClick={AddTodo}
             className="text-black bg-white px-4 py-2 rounded-3xl shadow-[0_0_10px_white] transition-all duration-300 ease-in-out 
-              hover:bg-white/10 hover:text-white hover:backdrop-blur-md hover:border hover:border-white/40"
+              hover:bg-white/10 hover:text-white hover:backdrop-blur-md hover:border hover:border-white/40 font-bold"
           >
             Add
           </button>
@@ -74,6 +80,12 @@ export default function Todo() {
               key={index}
               className="w-full bg-white text-black px-4 py-3 rounded-xl shadow-md flex justify-between items-center"
             >
+              <button
+                onClick={() => handleClick(index)}
+                className={`w-16 h-16 rounded-full flex items-center justify-center cursor-pointer transition-all duration-300 
+            ${clicked === index ? "bg-green-500 text-white" : "bg-gray-300"}`}
+              >
+              </button>
               <span className="break-words max-w-[70%]">{todo}</span>
               <button
                 onClick={() => handelDelete(index)}
@@ -81,6 +93,7 @@ export default function Todo() {
               >
                 ✕
               </button>
+
             </li>
           ))}
         </ul>
